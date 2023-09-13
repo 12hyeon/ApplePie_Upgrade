a7b5018fcc9a445f3e3db7166548a4934e4a22de
Hyeonjeong Lee <hyeonjle@c10r8s7.42seoul.kr>
2023-09-13 13:21:55 +0900

    Fix: Mock test

--- src/test/java/capstone/ApplePie_Spring/User/service/UserServiceImplTest.java
+++ src/test/java/capstone/ApplePie_Spring/User/service/UserServiceImplTest.java
@@ -10,15 +10,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
-@Rollback
+// @SpringBootTest // 정의된 모든 빈을 생성 -> 통합 테스트에 이용
+@RunWith(MockitoJUnitRunner.class) // Mock 사용할 수 있게
+@TestPropertySource(locations = "/application-test.properties") // 다른 db와 연결
 class UserServiceImplTest {
-    @Autowired private UserRepository userRepository;
+    
+    @MockBean // 테스트 분리 -> 단위 테스트 : 실제 동작에 관여하지 않는 빈은 필요 없음
+    private UserRepository userRepository;
 
+    @MockBean // Spring context에 올라감
+    private ProfileRepository profileRepository;
 
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService = new UserServiceImpl(userRepository, profileRepository, passwordEncoder);
+
+    @DisplayName("존재하는 유저는 프로필 조회가 가능하다.")
     @Test
     public void saveUser() {
+
+        // given
         SignupDto signupDto = SignupDto.builder()
                 .email("test")
                 .password("test")
@@ -28,7 +41,12 @@ class UserServiceImplTest {
                 .build();
 
         User user = signupDto.toUser();
-        User findUser = userRepository.save(user);
+        User saveUser = userService.save(user);
+
+        // when
+        User findUser = userService.findProfile(saveUser.getId());
+
+        // then
         Assertions.assertThat(findUser.getNickname().equals("test"));
 
     }
