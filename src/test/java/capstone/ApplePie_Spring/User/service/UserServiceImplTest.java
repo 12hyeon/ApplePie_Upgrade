2919d940319025e7c61f84e0489378f3ece6d64a
Hyeonjeong Lee <hyeonjle@c10r8s7.42seoul.kr>
2023-09-13 13:36:00 +0900

    Add: mock dependencies

--- src/test/java/capstone/ApplePie_Spring/User/service/UserServiceImplTest.java
+++ src/test/java/capstone/ApplePie_Spring/User/service/UserServiceImplTest.java
@@ -6,22 +6,24 @@ import capstone.ApplePie_Spring.User.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // @SpringBootTest // 정의된 모든 빈을 생성 -> 통합 테스트에 이용
 @RunWith(MockitoJUnitRunner.class) // Mock 사용할 수 있게
 @TestPropertySource(locations = "/application-test.properties") // 다른 db와 연결
 class UserServiceImplTest {
     
-    @MockBean // 테스트 분리 -> 단위 테스트 : 실제 동작에 관여하지 않는 빈은 필요 없음
+    @Mock // 테스트 분리 -> 단위 테스트 : 실제 동작에 관여하지 않는 빈은 필요 없음
     private UserRepository userRepository;
 
-    @MockBean // Spring context에 올라감
+    @Mock // Spring context에 올라감
     private ProfileRepository profileRepository;
 
-    @MockBean
+    @Mock
     private PasswordEncoder passwordEncoder;
 
     @InjectMocks
